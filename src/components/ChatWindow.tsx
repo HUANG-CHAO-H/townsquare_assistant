8c5cf7b9c4b1bbeaa16de8f23e81677577b31f0c
黄超 <huangchao.hello@bytedance.com>
2022-08-26 19:01:00 +0800

    简陋的滚动条自动滚动

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,19 +1,23 @@
 import {Layout, Row, Col, TextArea, Button, Avatar} from "@douyinfe/semi-ui";
 import {usePageState} from "./PageStateProvider";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {sendMessage} from "../typescript/血染钟楼助手(base)";
-
-const { Header, Footer, Content } = Layout;
 
 export function ChatWindow() {
     const pageState = usePageState();
     const [inputValue, setInputValue] = useState<string>('');
-
+    const divRef = useRef<HTMLDivElement | null>(null);
 
     const onButtonClick = () => {
         if (!inputValue) return;
         sendMessage(playerSeat + 1, inputValue, true)
-            .then(() => setInputValue(''));
+            .then(() => {
+                setInputValue('');
+                setTimeout(() => {
+                    const div = divRef.current;
+                    if (div) div.scrollTop = div.scrollHeight;
+                }, 500);
+            });
     }
 
     if (!pageState?.gameState?.players) return null;
@@ -33,10 +37,10 @@ export function ChatWindow() {
             <br/>
             <div>自定义功能列表</div>
             <br/>
-            <div style={contentStyle} dangerouslySetInnerHTML={{__html: pageState?.chatContent || ''}} />
+            <div ref={divRef} style={contentStyle} dangerouslySetInnerHTML={{__html: pageState?.chatContent || ''}} />
             <br/>
             <Row gutter={16} type="flex" align="middle">
-                <Col span={20}><TextArea value={inputValue} onChange={setInputValue}/></Col>
+                <Col span={20}><TextArea value={inputValue} onChange={setInputValue} onEnterPress={onButtonClick}/></Col>
                 <Col span={4}><Button block={true} onClick={onButtonClick}>发送</Button></Col>
             </Row>
         </Layout>
